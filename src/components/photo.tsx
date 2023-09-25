df4e1e0da3f77fe5f6bdebf0dd0f3d1909232265
Brendan Probst <bprobst@plmfleet.com>
2023-09-25 16:40:05 -0400

    trouble shooting images

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -5,12 +5,14 @@ export default function Photo({ post }: { post: PostSchemaType }) {
   return (
     <div className="bg-gray-800 flex flex-col p-2 pb-4 rounded-lg gap-2 h-full w-full">
       <div className="w-56 relative h-60 overflow-hidden rounded-lg flex flex-col justify-start items-start bg-slate-900 z-1">
-        <img
-          src={post.imageUrl}
-          alt={post.imageAlt}
-          style={{ objectFit: "contain" }}
-          className="rounded-md h-48"
-        />
+        <a href={post.imageUrl} className={"border-blue border-2"}>
+          <img
+            src={post.imageUrl}
+            alt={post.imageAlt}
+            style={{ objectFit: "contain" }}
+            className="rounded-md h-48"
+          />
+        </a>
         <p>{post.imageUrl}</p>
       </div>
       <Text fontSize={"md"} color={"gray.300"}>

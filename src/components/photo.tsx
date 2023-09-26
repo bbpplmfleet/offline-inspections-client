3835927e5c6d3d2f363d3c464a3ab905baa6639e
Brendan Probst <bprobst@plmfleet.com>
2023-09-26 08:36:48 -0400

    image link

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -4,17 +4,19 @@ import { Text } from "@chakra-ui/react";
 export default function Photo({ post }: { post: PostSchemaType }) {
   return (
     <div className="bg-gray-800 flex flex-col p-2 pb-4 rounded-lg gap-2 h-full w-full">
-      <div className="w-56 relative h-60 overflow-hidden rounded-lg flex flex-col justify-start items-start bg-slate-900 z-1">
-        <a href={post.imageUrl} className={"border-blue border-2"}>
-          <img
-            src={post.imageUrl}
-            alt={post.imageAlt}
-            style={{ objectFit: "contain" }}
-            className="rounded-md h-48"
-          />
-        </a>
+      <a href={post.imageUrl} className={"border-blue border-2"}>
+        <Text>link</Text>
+      </a>
+      {/* <div className="w-56 relative h-60 overflow-hidden rounded-lg flex flex-col justify-start items-start bg-slate-900 z-1">
+        <img
+          src={post.imageUrl}
+          alt={post.imageAlt}
+          style={{ objectFit: "contain" }}
+          className="rounded-md h-48"
+        />
+
         <p>{post.imageUrl}</p>
-      </div>
+      </div> */}
       <Text fontSize={"md"} color={"gray.300"}>
         {post.caption || <i>No Caption</i>}
       </Text>
